3e6cc56f3c1ac363558790ab188bca0e206980a4
Agbaje Stephen <agbajestephen5@gmail.com>
2026-07-29 19:08:29 +0100

    Update main.jsx

--- alaffia-concierge/src/main.jsx
+++ alaffia-concierge/src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
 import './index.css'
 import App from './App.jsx'
 
-// Mount the React app into the DOM root element
+const CLERK_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={CLERK_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
