cc776a774f345d3fb648068fe7cbd18fad0f0c5a
Agbaje Stephen <agbajestephen5@gmail.com>
2026-07-31 15:15:24 +0100

    Create clerk.js

---
+++ Cultural Concierge/src/lib/clerk.js
@@ -0,0 +1,10 @@
+let getTokenFn = null
+
+export function setTokenProvider(fn) {
+  getTokenFn = fn
+}
+
+export async function getClerkToken() {
+  if (!getTokenFn) return null
+  return getTokenFn()
+}
