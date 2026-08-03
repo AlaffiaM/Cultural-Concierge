5368f06f6b7b48c1ce36e963891a4b23259c5242
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-03 05:51:33 +0100

    Update sanitize.js

--- Backend/src/utils/sanitize.js
+++ Backend/src/utils/sanitize.js
@@ -1,3 +1,4 @@
+// Escapes regex metacharacters so user input can't inject into $regex queries.
 function escapeRegex(str) {
   if (typeof str !== 'string') return ''
   return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
