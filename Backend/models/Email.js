742f8fe9c73f9790019de067607ace0e71aa1c13
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-03 03:07:09 +0100

    Delete Email.js

--- Backend/models/Email.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const emailSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  name: { type: String, default: '' },
-  source: { type: String, enum: ['signin', 'newsletter'], default: 'signin' },
-}, { timestamps: true })
-
-module.exports = mongoose.model('Email', emailSchema)
